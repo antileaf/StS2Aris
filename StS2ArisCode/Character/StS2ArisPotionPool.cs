474a577a405b3f04d48a554edb0588e6edca221c
kimjoy2002 <kimjoy2002@naver.com>
2026-05-29 01:37:44 +0900

    우선 적용된 부분 수정

--- StS2ArisCode/Character/StS2ArisPotionPool.cs
+++ StS2ArisCode/Character/StS2ArisPotionPool.cs
@@ -9,6 +9,6 @@ public class StS2ArisPotionPool : CustomPotionPoolModel
     public override Color LabOutlineColor => StS2Aris.Color;
 
 
-    public override string BigEnergyIconPath => "charui/big_energy.png".ImagePath();
-    public override string TextEnergyIconPath => "charui/text_energy.png".ImagePath();
+    public override string BigEnergyIconPath => "charui/aris_energy.png".ImagePath();
+    public override string TextEnergyIconPath => "charui/aris_text_energy.png".ImagePath();
 }
